50a555134db5f4c638f643729d4eab4574132a40
altitor <brodymich608@gmail.com>
2024-06-30 18:47:50 -0400

    Add files via upload

---
+++ PYConsle_1.py
@@ -0,0 +1,14 @@
+# Made my altitoroby.iso
+ph = "Hello World"
+h = "• h = prints a help menu.                     • h_advanced = prints a more advanced menu.                                               -more coming soon. sorry!"
+h_advanced = "soon. . ."
+print("Welcome to the PYConsle!!")
+print("Its in beta 0.1 so not much yet")
+print("Type 'h' to get more commands.")
+Command_1 = input("➡️ ")
+if Command_1 == ("h"):
+    print(h)
+if Command_1 == ("h_advanced"):
+    print(h_advanced)
+if Command_1 == ("ph"):
+    print(ph)
